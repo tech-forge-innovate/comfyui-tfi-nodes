2b5c63f9dcb75495995d873e57f47aacdf431179
Swapnil Kumbhar <swapkumbhar31@gmail.com>
2026-02-02 19:13:54 -0500

    Remove unused timer and webhook nodes to streamline functionality

--- __init__.py
+++ __init__.py
@@ -1,24 +1,15 @@
-from .nodes.webhook import *
-from .nodes.start_timer import *
-from .nodes.execution_timer import *
 from .nodes.audio_url_loader import AudioURLLoader
 from .nodes.audio_duration import AudioDuration
 from .nodes.bunny_node import BunnyCDNStorageNodeVideoUpload
 
 NODE_CLASS_MAPPINGS = {
-    "TFI Webhook": Webhook,
-    "Start timer": StartTimer,
-    "Execution timer": ExecutionTimer,
     "Audio URL Loader": AudioURLLoader,
     "Audio duration": AudioDuration,
     "Bunny CDN Video Upload": BunnyCDNStorageNodeVideoUpload,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TFI Job Data": "💾 TFI Job Data",
-    "Start timer": "Start timer",
-    "Execution timer": "Execution timer"
-    ,"Audio URL Loader": "🔊 Audio URL Loader",
+    "Audio URL Loader": "🔊 Audio URL Loader",
     "Audio duration": "⏱️ Audio duration"
     ,"Bunny CDN Video Upload": "🐰 Bunny CDN Video Upload"
 }
